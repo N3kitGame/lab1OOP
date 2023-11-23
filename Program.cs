ac44198bb43942b0077bbe0cbda9076b69ed4d3a
Никита <@WIN-KNAFN3MS6M4>
2023-11-23 20:33:10 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using lab1;
+using System.Configuration;
+using System.Web;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        PrintWelcomeUser();
+        PrintWaitUser();
+        PrintGoodByeUser();
+    }
+
+    private static void PrintWelcomeUser()
+    {
+        Console.WriteLine(Resource1.Welcome + ConfigurationManager.AppSettings.Get("Name"));
+    }
+    private static void PrintWaitUser()
+    {
+        Console.WriteLine(Resource1.Wait);
+        Console.ReadLine();
+    }
+    
+    private static void PrintGoodByeUser()
+    {
+        Console.WriteLine(Resource1.GoodBye + ConfigurationManager.AppSettings.Get("Name"));
+    }
+}
